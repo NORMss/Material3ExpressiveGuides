68a66ecfdfbc2450572d61f6ff0008589388eb89
Sergey Bezborodov <serega.b301@gmail.com>
2026-02-06 00:51:47 +0700

    init commit

---
+++ app/src/main/java/ru/normno/material3expressiveguides/MainActivity.kt
@@ -0,0 +1,28 @@
+package ru.normno.material3expressiveguides
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.normno.material3expressiveguides.ui.theme.Material3ExpressiveGuidesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Material3ExpressiveGuidesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
